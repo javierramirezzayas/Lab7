2f0b072dd294cfd1793ea28e25f1f8655a615de3
4015c14 <4015c14@toronja.ece.uprm.edu>
2016-10-26 11:42:31 -0400

    First Commit

---
+++ src/Sign.java
@@ -0,0 +1,11 @@
+public class Sign implements GameThing
+{
+	public String getName()
+	{
+		return "A wooden sign";
+	}
+	public String getDescription()
+	{
+		return "This is a small wooden sign. There seems to be some partially faded writing in it.";
+	}
+}
