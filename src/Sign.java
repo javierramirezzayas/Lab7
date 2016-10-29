7285cb4140819b041b7122441af46352d773a877
javierramirezzayas <javier.ramirez1@upr.edu>
2016-10-28 22:59:07 -0400

    Second commit

--- src/Sign.java
+++ src/Sign.java
@@ -2,7 +2,7 @@ public class Sign implements GameThing, Readable, Edible
 {
 	public String getName()
 	{
-		return "A wooden sign  transformed into chocolate";
+		return "A wooden sign transformed into chocolate";
 	}
 	public String getDescription()
 	{
@@ -18,7 +18,7 @@ public class Sign implements GameThing, Readable, Edible
 	// Added for part 8
 	public void eat() {
 		AdventureEpsilon.theRoom.removeThing(AdventureEpsilon.theSign);
-		System.out.println("You eat the chocolate and start to see things you haven't seen before."); 
+		System.out.println("You eat the chocolate. Tastes like wood."); 
 	}
 	// Added for part 8
 }
